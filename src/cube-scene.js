5d463bb237c90f1a833df368d6148ac3648335b2
Mitch Allen <mitch.c.allen@gmail.com>
2020-12-24 15:33:23 -0500

    refactored argument handling

--- src/cube-scene.js
+++ src/cube-scene.js
@@ -5,28 +5,29 @@ import {CubeFactory} from './cube.js';
 export class CubeSceneFactory {
 
   // 3. Define a static create method to return new scenes filled with cubes
-  static create(spec) {
+  static create( spec = {}) {
 
     // 4. Setup default values or use spec arguments
-    spec = spec || {};
-    var clear = spec.clear || "#000000";
-    // fov — Camera frustum vertical field of view
-    var fov = spec.fov || 75;
-    // aspect — Camera frustum aspect ratio
-    var aspectRatio = spec.aspectRatio || window.innerWidth / window.innerHeight;
-    var near = spec.near || 0.1;  // near — Camera frustum near plane
-    var far = spec.far || 1000; // far — Camera frustum far plane.
+    let {
+      clear = "#000000",
+      // fov — Camera frustum vertical field of view
+      fov = 75,
+      // aspect — Camera frustum aspect ratio
+      aspectRatio = window.innerWidth / window.innerHeight,
+      near = 0.1,  // near — Camera frustum near plane
+      far = 1000, // far — Camera frustum far plane.
+    } = spec;
 
     // 5. Define a list of options for cubes to be created
     var cubeOptions = [
       { color: "#FF0000" },
-      { color: "#00FF00", width: 0.5, height: 2, depth: 0.5 },
-      { color: "#0000FF", width: 2, height: 0.5, depth: 0.5 },
-      { color: "#FF00FF", width: 0.5, height: 0.5, depth: 2 },
-      { color: "#FFFF00", translateX: 3.0 },
-      { color: "#FF6619", translateX: -3.0 },
-      { color: "#AAAAAA", translateY: 2.0, translateZ: -0.05, width: 0.5, height: 0.5, depth: 2 },
-      { color: "#04D9FF", translateY: -2.0, translateZ: 0.05, width: 0.5, height: 0.5, depth: 2 },
+      { color: "#00FF00", width: 0.5, height: 2.0, depth: 0.5 },
+      { color: "#0000FF", width: 2.0, height: 0.5, depth: 0.5 },
+      { color: "#FF00FF", width: 0.5, height: 0.5, depth: 2.0 },
+      { color: "#FFFF00", x: 3.0 },
+      { color: "#FF6619", x: -3.0 },
+      { color: "#AAAAAA", y: 2.0,  z: -0.05, width: 0.5, height: 0.5, depth: 2 },
+      { color: "#04D9FF", y: -2.0, z:  0.05, width: 0.5, height: 0.5, depth: 2 },
     ];
 
     // 6. Setup a ThreeJS renderer to render the scene
--- src/cube.js
+++ src/cube.js
@@ -2,18 +2,19 @@
 export class CubeFactory {
 
   // 2. Define a static create method to return new cubes
-  static create(spec) {
+  static create( spec = {} ) {
 
     // 3. Setup default values or use arguments
-    spec = spec || {};
-    var name = spec.name = "cube";  
-    var color = spec.color || "#FF00FF";
-    var width = spec.width || 1;
-    var height = spec.height || 1;
-    var depth = spec.depth || 1;
-    var translateX = spec.translateX || 0.0;
-    var translateY = spec.translateY || 0.0;
-    var translateZ = spec.translateZ || 0.0;
+    let {
+      name = "cube", 
+      color = "#FF00FF",
+      width = 1,
+      height = 1,
+      depth = 1,
+      x = 0.0,
+      y = 0.0,
+      z = 0.0,
+    } = spec;
 
     // 4. Use ThreeJS to define a 3D cube
     var geometry = new THREE.BoxGeometry(width, height, depth);
@@ -24,9 +25,9 @@ export class CubeFactory {
     cube.name = name;
 
     // 6. Using ThreeJS methods on the cube, move it to a specific offset
-    cube.translateX(translateX);
-    cube.translateY(translateY);
-    cube.translateZ(translateZ);
+    cube.translateX(x);
+    cube.translateY(y);
+    cube.translateZ(z);
 
     // 7. Return the new cube
     return cube;
